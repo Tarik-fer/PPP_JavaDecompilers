449ffc8fec8127113b387e5878c9a9e0c83ff5fd
Tarik-fer <tarik.karamehmedovic@gmail.com>
2020-01-21 20:06:07 +0100

    Dodano ocjenivanje datoteke

--- dekompajlerSkripta.py
+++ dekompajlerSkripta.py
@@ -4,6 +4,7 @@ import zipfile
 import subprocess
 import datetime
 import hashlib
+import re
 
 #dictionary dekompilera
 decompilers = {
@@ -34,6 +35,47 @@ def extractFile(sourcePath, destPath):
 	print("DONE")
 	return
 
+def countVariableInCondition(line):
+	#micanje svih posebnih znakova i stringova koji bi se mogli naci unutar linije uvjeta
+	line = line.replace("}", " ")
+	line = line.replace("{", " ")
+	line = line.replace("if", "")
+	line = line.replace("else if", "")
+	line = line.replace("<", " ")
+	line = line.replace(">", " ")
+	line = line.replace(">=", " ")
+	line = line.replace("<=", " ")
+	line = line.replace("&&", " ")
+	line = line.replace("||", " ")
+	line = line.replace("!", " ")
+	line = line.replace("(", " ")
+	line = line.replace(")", " ")
+	#micanje visestrukih razmaka
+	line = ' '.join(line.split())
+	#vraca se broj varijabli koje se koriste unutar samog uvjeta
+	return len(line.split())
+
+def getFlowStatementComplexity(line):
+	#svaka usporedba se mjeri tezinom od 0.5 'jedinica'
+	complexity = float(line.count("<") + line.count(">") + line.count(">=") + line.count("<=") + line.count("==") + line.count("!"))*0.5
+	# print("got a ! over here: %.1f" % complexity)
+	complexity += line.count("||") + line.count("&&")
+	# print("after or and: %.1f" % complexity)
+	complexity += countVariableInCondition(line)
+	return complexity
+
+
+def countConditionStatements(filePath):
+	statementCounter = 0
+	with open(filePath) as file:
+		for line in file:
+			if "continue" in line or "break" in line:
+				statementCounter += 1
+			if ("if" in line or "else if" in line) and ";" not in line:
+				statementCounter += float(getFlowStatementComplexity(line))
+			if "for" in line and ";" not in line:
+				statementCounter += 1
+	return statementCounter
 
 ################################################################## winnowing
 def h11(word):
@@ -85,11 +127,11 @@ while action not in ["1", "2"]:
 
 if action == "1":
 	# input naziva decompilera
-	decompiler = raw_input("Unesite dekompajler (cfr, procyon, fernflower): ")#sys.argv[1]
+	decompiler = raw_input("Unesite naziv dekompajlera (cfr, procyon, fernflower): ")#sys.argv[1]
 	while decompiler not in ["cfr", "procyon", "fernflower"]:
-		decompiler = raw_input("Unesite dekompajler (cfr, procyon, fernflower): ")#sys.argv[1]
+		decompiler = raw_input("Unesite naziv dekompajlera (cfr, procyon, fernflower): ")#sys.argv[1]
 	# stvarcica sa jarom
-	programPath = raw_input("Unesite relativni path do jar datoteke (datoteka se mora nalaziti unutar test_programi, path je tipa ./test_programi/...): ")#sys.argv[2]
+	programPath = raw_input("Unesite path do jar ili class datoteke: ")#sys.argv[2]
 	# naziv folder u koji ide output, nije cijeli path
 	outputFolder = raw_input("Unesite naziv foldera (ne cijeli path) u kojem ce se nalaziti rjesenje: ")#sys.argv[3]
 		###################################
@@ -109,18 +151,18 @@ if action == "1":
 		pass
 
 	if decompiler == "procyon":
-		print("DECOMPILER PATH: "+decompilerPath)
-		print("PROGRAM PATH: "+programPath)
-		print("OUTPUT PATH: "+outputPath)
+		# print("DECOMPILER PATH: "+decompilerPath)
+		# print("PROGRAM PATH: "+programPath)
+		# print("OUTPUT PATH: "+outputPath)
 		command = "java -jar %s \"%s\" -o \"%s\"" % (decompilerPath, programPath, outputPath)
-		print("COMMAND: "+command)
+		# print("COMMAND: "+command)
 		# subprocess.call(command, shell=true)
 		os.system(command)
 
 	# fernflower ima problem da pri dekompajliranju .jar datoteke on svoj rezultat sprema u isto imenu .jar datoteku,
 	# pa ju moramo extractati iz arhive kako bi mogli vidjeti .java datoteke
 	if decompiler == "fernflower":
-		command = "java -jar %s %s %s" % (decompilerPath, programPath, outputPath)
+		command = "java -jar %s \"%s\" \"%s\"" % (decompilerPath, programPath, outputPath)
 		if not os.path.exists(outputPath):
 			os.makedirs(outputPath)
 		os.system(command)
@@ -132,32 +174,46 @@ else:
 	originalFilePath = raw_input("Unesite path do originalne .java datoteke: ")
 	originalJavaFile = open(originalFilePath, "r").read()
 	originalFileStats = os.stat(originalFilePath)
-	decompiledFilePath = raw_input("Unesite path do dekompajlirane .java datoteke: ")
-	decompiledJavaFile = open(decompiledFilePath, "r").read()
-	decompiledFileStats = os.stat(decompiledFilePath)
+	secondFilePath = raw_input("Unesite path do dekompajlirane .java datoteke: ")
+	secondJavaFile = open(secondFilePath, "r").read()
+	secondFileStats = os.stat(secondFilePath)
 
-	print("First file size: %s bytes" % originalFileStats.st_size)
-	print("Second file size: %s bytes" % decompiledFileStats.st_size)
+	print("Za sva ocjenivanja, ocjena 1 se daje prvoj datoteci te se uzima kao baza ocjene druge datoteke (veca ocjena predstavlja bolji program)")
+
+	print("Ocjena velicine datoteka:")
+	#ako druga datoteka ima vecu velicinu, imat ce manju ocjenu
+	sizeGrade = float(originalFileStats.st_size) / float(secondFileStats.st_size)
+	print("\tOcjena prve datoteke: 1")
+	print("\tOcjena druge datoteke: %.5f" % sizeGrade)
+
+	print("Ocjena kompleksnosti uvjeta upravljanja tokom:")
+	originalFlowStatements = countConditionStatements(originalFilePath)
+	secondFlowStatements = countConditionStatements(secondFilePath)
+	#ako druga datoteka ima vise kompliciraniji statement, imat ce veci broj bodava i time manju ocjenu
+	controlFlowStatementGrade = float(originalFlowStatements) / float(secondFlowStatements)
+	print("\tOcjena prve detoteke: 1")
+	print("\tOcjena druge datoteke: %.5f" % controlFlowStatementGrade)
 
 	originalFingerprint = createFingerprints(originalJavaFile, 10)
-	decompiledFingerprint = createFingerprints(decompiledJavaFile, 10)
+	secondFingerprint = createFingerprints(secondJavaFile, 10)
 
 	winnowingOriginal = winnowingAlgorithm(originalFingerprint, 40)
-	winnowingDecompiled = winnowingAlgorithm(decompiledFingerprint, 40)
+	winnowingSecond = winnowingAlgorithm(secondFingerprint, 40)
 
 	intersectionCounter = 0;
-	for fingerprint in winnowingDecompiled:
+	for fingerprint in winnowingSecond:
 	    if fingerprint in winnowingOriginal:
 	        intersectionCounter += 1;
 
 	for fingerprint in winnowingOriginal:
-	    if fingerprint in winnowingDecompiled:
+	    if fingerprint in winnowingSecond:
 	        intersectionCounter += 1;
 
-	print(str(len(winnowingOriginal) + len(winnowingDecompiled)) + " otisaka ukupno.")
-	print(str(intersectionCounter) +  " zajednickih otisaka.")
-	result = (float(intersectionCounter) / (float(len(winnowingOriginal)) + float(len(winnowingDecompiled)))) * 100
-	print("Slicnost: %.3f%%" % result)
+	print("Wennowing slicnost:")
+	print("\t"+ str(len(winnowingOriginal) + len(winnowingSecond)) +" otisaka ukupno.")
+	print("\t"+ str(intersectionCounter) +  " zajednickih otisaka.")
+	result = (float(intersectionCounter) / (float(len(winnowingOriginal)) + float(len(winnowingSecond)))) * 100
+	print("\tslicnost: %.3f%%" % result)
 
 
 
