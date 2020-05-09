00e19fc91ad8a1e57ceb295764f790b23ef5bd20
Tarik-fer <tarik.karamehmedovic@gmail.com>
2020-05-09 23:54:48 +0200

    Raspodijeljeni nacini usporedbe (original vs dekompilirano i usporedba dva dekompilirana programa). Dodana metoda za usporedbu po halstead metrikama

--- Compare.py
+++ Compare.py
@@ -94,6 +94,44 @@ def printWinnowingSimilarity(winnowingOriginal, winnowingSecond, intersectionCou
 	print("\tslicnost: %.3f%%" % result)
 
 
+def printHalsteadComparison(first_file_h_metric: MetricProfiler, second_file_h_metric: MetricProfiler):
+	global compare_method
+
+	# print("Halstead volumen prve datoteke: " + str(first_file_h_metric.h_volume))
+	# print("Halstead volumen druge datoteke: " + str(second_file_h_metric.h_volume))
+	# print("Halstead tezina prve datoteke: " + str(first_file_h_metric.h_difficulty))
+	# print("Halstead tezina druge datoteke: " + str(second_file_h_metric.h_difficulty))
+	volume_relative_diff = first_file_h_metric.h_volume / second_file_h_metric.h_volume
+	if volume_relative_diff > 1:
+		volume_relative_diff = 1 / volume_relative_diff
+
+	difficulty_relative_diff = first_file_h_metric.h_difficulty / second_file_h_metric.h_difficulty
+	if difficulty_relative_diff > 1:
+		difficulty_relative_diff = 1 / difficulty_relative_diff
+
+	if first_file_h_metric.h_difficulty > second_file_h_metric.h_difficulty:
+		print(("Originalna" if compare_method == ORIGINAL_vs_DECOMPILE else "Prva") +
+		      " datoteka je teza za razumjeti te je dobila slijedecu ocjenu naspram drugoj: " +
+		      str(round(difficulty_relative_diff * 100, 3)) + "%")
+	elif first_file_h_metric.h_difficulty < second_file_h_metric.h_difficulty:
+		print(("Dekompilirana" if compare_method == ORIGINAL_vs_DECOMPILE else "Druga") +
+		      " datoteka je teza za razumjeti te je dobila slijedecu ocjenu naspram prvoj: " +
+		      str(round(difficulty_relative_diff * 100, 3)) + "%")
+	else:
+		print("Datoteke su jednako teske za razumjeti te imaju istu ocjenu")
+
+	if first_file_h_metric.h_volume > second_file_h_metric.h_volume:
+		print("Za " + ("originalnu" if compare_method == ORIGINAL_vs_DECOMPILE else "prvu") +
+		      " datoteku je potrebno vise vremena kako bi se razumjela te je dobila slijedecu ocjenu naspram drugoj: " +
+		      str(round(volume_relative_diff * 100, 3)) + "%")
+	elif first_file_h_metric.h_volume < second_file_h_metric.h_volume:
+		print("Za " + ("dekompiliranu" if compare_method == ORIGINAL_vs_DECOMPILE else "drugu") +
+		      " datoteku je potrebno vise vremena kako bi se razumjela te je dobila slijedecu ocjenu naspram prvoj: " +
+		      str(round(volume_relative_diff * 100, 3)) + "%")
+	else:
+		print("Potrebno je podjednako vremena kako bi se datoteke razumjele te imaju istu ocjenu")
+
+
 def printHalsteadMetric(file_h_metric):
 	print("First file halstead :    (" +
 	      str(file_h_metric.line_num) + ", " +
@@ -117,40 +155,52 @@ def printHalsteadSimilarity(first_file_h_metric, second_file_h_metric):
 	      str(abs(first_file_h_metric.h_vocabulary - second_file_h_metric.h_vocabulary)) + ")")
 
 	physical_dif = first_file_h_metric.comparePhysicalProfileWith(second_file_h_metric)
-	print("Slicnost fizickog profila druge datoteke naspram prvoj : " + str(round(physical_dif * 100, 3)) + "%")
+	print("Slicnost fizickog profila : " + str(round(physical_dif * 100, 3)) + "%")
 
-	if first_file_h_metric.h_volume > second_file_h_metric.h_volume:
-		print("Prva datoteka ima slozeniji programski kod")
-	else:
-		print("Druga datoteka ima slozeniji programski kod")
+
+def printCompositeSimilarity(winnowing_similarity, physical_similarity, halstead_similarity):
+	weighted_winnowing = winnowing_similarity * 0.6
+	weighted_halstead = halstead_similarity * 0.3
+	weighted_physical = physical_similarity * 0.1
+	composite_profile = weighted_winnowing + weighted_halstead + weighted_physical
+	print("Ukupna ocjena slicnosti : " + str(round(composite_profile * 100, 3)) + "%")
+
+
+ORIGINAL_vs_DECOMPILE = 1
+DECOMPILED_FILES = 2
+compare_method = None
 
 
 def compare():
+	global ORIGINAL_vs_DECOMPILE
+	global DECOMPILED_FILES
+	global compare_method
 
-	# original vs dekompilirano
-	# dekompilirano vs drugi dekompilirano
-	# samo jedan file metrike
+	compare_method = input("Nacin usporedbe:\n  (1) Original i dekompilirano\n  (2) Dvije Dekompilirane datoteke\n\tOdaberite 1 ili 2: ")
+	while compare_method not in ["1", "2"]:
+		compare_method = input(
+			"Nacin usporedbe:\n  (1) Original i dekompilirano\n  (2) Dvije Dekompilirane datoteke\n\tOdaberite 1 ili 2: ")
+	compare_method = int(compare_method)
 
-	print("\nUsporedba dekompajliranih datoteka:")
-	firstFilePath = input("Unesite path do originalne .java datoteke: ")
+	print("\nUsporedba datoteka:")
+	if compare_method == ORIGINAL_vs_DECOMPILE:
+		firstFilePath = input("Unesite path do originalne .java datoteke: ")
+	else:
+		firstFilePath = input("Unesite path do prve .java datoteke: ")
 	first_file_content = open(firstFilePath, "r", encoding='utf8').read()
 	firstFileStats = os.stat(firstFilePath)
-	og_b4_token = first_file_content
+	first_before_token = first_file_content
 
-	secondFilePath = input("Unesite path do dekompajlirane .java datoteke: ")
+	if compare_method == ORIGINAL_vs_DECOMPILE:
+		secondFilePath = input("Unesite path do dekompajlirane .java datoteke: ")
+	else:
+		secondFilePath = input("Unesite path do druge .java datoteke: ")
 	second_file_content = open(secondFilePath, "r", encoding='utf8').read()
 	secondFileStats = os.stat(secondFilePath)
+	second_before_token = second_file_content
 
-	# print(
-	# 	"Za sva ocjenivanja, ocjena 1 se daje prvoj datoteci te se uzima kao baza ocjene druge datoteke (veca ocjena predstavlja bolji program)")
-	#
-	# print("Ocjena velicine datoteka:")
-	# ako druga datoteka ima vecu velicinu, imat ce manju ocjenu
 	sizeGrade = float(firstFileStats.st_size) / float(secondFileStats.st_size)
-	# print("\tOcjena prve datoteke: 1")
-	# print("\tOcjena druge datoteke: %.5f" % sizeGrade)
 
-	# print("Ocjena kompleksnosti uvjeta upravljanja tokom:")
 	originalFlowStatements = countConditionStatements(firstFilePath)
 	secondFlowStatements = countConditionStatements(secondFilePath)
 
@@ -159,14 +209,6 @@ def compare():
 		controlFlowStatementGrade = float(originalFlowStatements) / float(secondFlowStatements)
 	else:
 		controlFlowStatementGrade = 0
-	# print("\tOcjena prve detoteke: 1")
-	# print("\tOcjena druge datoteke: %.5f" % controlFlowStatementGrade)
-
-	# print("Before process: " + first_file_content)
-	# first_file_content = process_input(first_file_content)            # PROCESSING
-	# second_file_content = process_input(second_file_content)
-	# # print("-----------------------------------------------------------------------------------------")
-	# print("After process: " + first_file_content)
 
 	first_file_h_metric = MetricProfiler(first_file_content)
 	print("-------------------------------------------------------------")
@@ -189,46 +231,20 @@ def compare():
 		if fingerprint in winnowingSecond:
 			intersectionCounter += 1
 
-	printStatsGrade(sizeGrade, controlFlowStatementGrade)
+	if compare_method == ORIGINAL_vs_DECOMPILE:
+		printStatsGrade(sizeGrade, controlFlowStatementGrade)
+		printWinnowingSimilarity(winnowingOriginal, winnowingSecond, intersectionCounter)
+		printHalsteadSimilarity(first_file_h_metric, second_file_h_metric)
 
-	printWinnowingSimilarity(winnowingOriginal, winnowingSecond, intersectionCounter)
+		printHalsteadComparison(first_file_h_metric, second_file_h_metric)
 
-	printHalsteadSimilarity(first_file_h_metric, second_file_h_metric)
+		winnowing_similarity = (float(intersectionCounter) / (float(len(winnowingOriginal)) + float(len(winnowingSecond))))
+		physical_similarity = first_file_h_metric.comparePhysicalProfileWith(second_file_h_metric)
+		halstead_similarity = first_file_h_metric.h_vocabulary / second_file_h_metric.h_vocabulary
+		if halstead_similarity > 1:
+			halstead_similarity = 1 / halstead_similarity
+		printCompositeSimilarity(winnowing_similarity, physical_similarity, halstead_similarity)
 
-	# print("Winnowing slicnost:")
-	# print("\t" + str(len(winnowingOriginal) + len(winnowingSecond)) + " otisaka ukupno.")
-	# print("\t" + str(intersectionCounter) + " zajednickih otisaka.")
-	# result = (float(intersectionCounter) / (float(len(winnowingOriginal)) + float(len(winnowingSecond)))) * 100
-	# print("\tslicnost: %.3f%%" % result)
-
-	# print("                         (lines, words, chars, h_len, h_vol, h_vocabulary)")
-	# print("First file halstead :    (" +
-	#       str(first_file_h_metric.line_num) + ", " +
-	#       str(first_file_h_metric.word_num) + ", " +
-	#       str(first_file_h_metric.char_num) + ", " +
-	#       str(first_file_h_metric.h_length) + ", " +
-	#       str(round(first_file_h_metric.h_volume, 3)) + ", " +
-	#       str(first_file_h_metric.h_vocabulary) + ")")
-	#
-	# print("Second file halstead :   (" +
-	#       str(second_file_h_metric.line_num) + ", " +
-	#       str(second_file_h_metric.word_num) + ", " +
-	#       str(second_file_h_metric.char_num) + ", " +
-	#       str(second_file_h_metric.h_length) + ", " +
-	#       str(round(second_file_h_metric.h_volume, 3)) + ", " +
-	#       str(second_file_h_metric.h_vocabulary) + ")")
-	# print("               delta :   (" +
-	#       str(abs(first_file_h_metric.line_num - second_file_h_metric.line_num)) + ", " +
-	#       str(abs(first_file_h_metric.word_num - second_file_h_metric.word_num)) + ", " +
-	#       str(abs(first_file_h_metric.char_num - second_file_h_metric.char_num)) + ", " +
-	#       str(abs(first_file_h_metric.h_length - second_file_h_metric.h_length)) + ", " +
-	#       str(abs(round(first_file_h_metric.h_volume - second_file_h_metric.h_volume, 3))) + ", " +
-	#       str(abs(first_file_h_metric.h_vocabulary - second_file_h_metric.h_vocabulary)) + ")")
-	#
-	# physical_dif = first_file_h_metric.comparePhysicalProfileWith(second_file_h_metric)
-	# print("Slicnost fizickog profila druge datoteke naspram prvoj : " + str(round(physical_dif * 100, 3)) + "%")
-	#
-	# if first_file_h_metric.h_volume > second_file_h_metric.h_volume:
-	# 	print("Prva datoteka ima slozeniji programski kod")
-	# else:
-	# 	print("Druga datoteka ima slozeniji programski kod")
+	elif compare_method == DECOMPILED_FILES:
+		printStatsGrade(sizeGrade, controlFlowStatementGrade)
+		printHalsteadComparison(first_file_h_metric, second_file_h_metric)
